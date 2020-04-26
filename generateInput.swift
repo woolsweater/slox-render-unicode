5946f088331d87841567daabcb1e1e2ac75b0e47
Josh Caswell <josh@scylla>
2020-04-25 19:40:47 -0700

    Avoid generating reserved codepoints
    The Swift compiler won't accept this range in string literal escape
    sequences, so we can't compare output.

--- generateInput.swift
+++ generateInput.swift
@@ -5,25 +5,32 @@ let words = [
     "lorem", "ipsum", "dolor", "sit", "amet",
     "it", "was", "the", "best", "of", "times", "worst",
     "past", "is", "a", "foreign", "country",
-    "call", "me", "ishmael",
+    "call", "me", "Ishmael",
     "frog", "blast", "vent", "core",
     "phosphoglyceraldehyde", "supercalifragilisticexpialidocious",
-    "antidisestablishmentarianism"
+    "antidisestablishmentarianism",
+    "I", "am", "very", "model", "modern", "major", "general",
 ]
 
 /**
- Create a Unicode escape of the form \uNNNNNNNN for a random code point and
+ Create a Unicode escape of the form \u{NNNNNNNN} for a random code point and
  with a random amount of initial 0 padding.
  */
 func randomUnicodeEscape() -> String {
-    let point: UInt32
-    if Bool.random() {
-        // Start at printable ASCII chars
-        point = UInt32.random(in: 0x20...0xD7FF)
-    } else {
-        point = UInt32.random(in: 0xE000...0x10FFF)
+    // These codepoints cannot be used in Swift string literals:
+    // https://github.com/apple/swift/blob/a11cc4fcfc1ae8b30a48e246e983d3ffc5121d79/lib/Parse/Lexer.cpp#L74
+    let reservedRange: ClosedRange<UInt32> = 0xFDD0...0xFDEF
+    var point: UInt32 = 0xFDD0
+    while reservedRange ~= point {
+        if Bool.random() {
+            // Start at printable ASCII chars
+            point = UInt32.random(in: 0x20...0xD7FF)
+        } else {
+            point = UInt32.random(in: 0xE000...0x10FFF)
+        }
     }
-    return String(format: #"\u{%*0x}"#, Int.random(in: 2...8), point)
+
+    return String(format: #"\u{%*0x}"#, Int.random(in: 1...8), point)
 }
 
 /**
@@ -36,7 +43,6 @@ func randomSegment() -> String {
     return choices.joined(separator: " ")
 }
 
-//TODO: Render the text from Swift, too, so that the ouput can be compared
 var output = randomSegment()
 let segmentCount = Int(ProcessInfo.processInfo.arguments[1])!
 for _ in 0..<segmentCount {
